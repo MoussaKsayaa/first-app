320304f7b451c354bb0da02fc1b4a7b00caedd9b
MoussaKsayaa <moussaksayaa.dev@gmail.com>
2023-08-05 10:05:45 +0300

    first app

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import DateTime from "./components/DateTime";
+import "./App.css"
+
+function App() {
+    return (
+        <>
+        <header>
+            <h1>Date And Time</h1>
+        </header>
+        <main>
+            <DateTime />
+        </main>
+        </>
+
+    )
+}
+export default App;
---
+++ src/components/DateTime.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import HappySunImage from "./../images/happy-sun.jpg";
+import HappyMoonImage from "./../images/happy-moon.jpg";
+import "./DateTime.css";
+
+function DateTime() {
+    const date = new Date();
+    const day = date.toLocaleString("en-us", {weekday: "long"});
+    const morning = date.getHours() >= 6 && date.getHours() <= 12;
+    let dayMessage;
+    if (day.toLowerCase() === "monday") {
+        dayMessage = `Happy ${day}`;
+    } else if (day.toLowerCase() === "tuesday") {
+        dayMessage = `${day}, four days to go`;
+    } else if (day.toLowerCase() === "wednesday") {
+        dayMessage = `${day}, half way there`;
+    } else if (day.toLowerCase() === "thursday") {
+        dayMessage = `${day}, start planning the weekend.`;
+    } else if (day.toLowerCase() === "friday") {
+        dayMessage = `Woo - hoo, the weekend is coming!`;
+    } else {
+        dayMessage = "stay calm and keep having fun";
+    }
+    function IsItMorning() {
+        if (morning) {
+            return (
+            <>
+                <p>Have you had breakfast yet?</p>
+                <style>
+                    {`body { background-image: url(${HappySunImage}); }`}
+                </style>
+            </>)
+        } else {
+            return (
+            <>
+                <p>Have you had dinner yet</p>
+                <style>
+                    {`body { background-image: url(${HappyMoonImage});}`}
+                </style>
+            </>)
+        }
+    }
+    return (
+        <div>
+            <h3>{dayMessage}</h3>
+            <div>{IsItMorning()}</div>
+        </div>
+    )
+}
+export default DateTime;
